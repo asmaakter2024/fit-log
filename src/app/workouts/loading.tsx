44307fe3a5afb896a2fb1c57541201c03bdfc867
asmaakter2024 <tuly.asma@gmail.com>
2026-09-26 20:57:50 +0600

    loading msg added.please show that.

--- src/app/workouts/loading.tsx
+++ src/app/workouts/loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-  return <p className="p-6 text-white">Loading...💖</p>;
+  return <p className="p-6 text-white">Loading...💖🏋️‍♀️💪</p>;
 }
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -35,6 +35,7 @@ const Navbar = () => {
             <li>
               <Link
                 href="/workouts"
+                prefetch={false}
                 className="rounded-full bg-lime-400/10 px-4 py-2 text-xs font-semibold text-lime-300"
               >
                 Workouts
